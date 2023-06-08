cab6122be8044751e129b0d9030beb0766354804
Jay-Atal <thejayatal@gmail.com>
2023-06-07 21:17:07 -0400

    Added the JsonUtils and MessageJson Fixes #9.

---
+++ src/main/java/json/MessageJson.java
@@ -0,0 +1,10 @@
+package json;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
+import cs3500.pa03.controller.ProxyController;
+
+public record MessageJson(@JsonProperty("method-name") String methodName,
+                                   @JsonProperty("arguments") JsonNode arguments) {
+
+}
